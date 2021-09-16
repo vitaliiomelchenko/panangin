fb724cd583210646095499a7ef4a714ea68db70d
Vitalii Omelchenko <vomelchenko@purpleplanet.com>
2021-09-16 10:35:54 +0300

    Close menu on resize

--- js/main.js
+++ js/main.js
@@ -6,7 +6,11 @@ $(document).ready(function() {
 	$('.menu-toggle').on("click",function(){
     $('.header').addClass('menu-active');
     $('body').addClass('menu-active');
-	})
+  });
+  $(window).on('resize', function(){
+    $('.header').removeClass('menu-active');
+    $('body').removeClass('menu-active');
+  });
 });
 
 $('.menu-item__parent').each(function(){
