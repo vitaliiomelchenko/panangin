a87349ac939fa67ad10e5fa22cdb3de72e6852bd
Vitalii Omelchenko <vomelchenko@purpleplanet.com>
2021-09-24 11:42:37 +0300

    FAq fix

--- template-parts/acf-blocks/faq.php
+++ template-parts/acf-blocks/faq.php
@@ -32,28 +32,21 @@
                             $questionFAQ = get_sub_field('questionFAQ');
                             $answerFAQ = get_sub_field('answerFAQ');
                         ?>
-<!--                             <div class="b-faq">
-								<div class="tab faq__item">
-									<input id="<?php echo $i++;?>" type="checkbox" name="tabs">
-									<label for="<?php echo $a++;?>">
-										<?php echo $questionFAQ ?> 
-									</label>
-									<a href="#" class="faq__title js-faq-title">
-										<div class="faq__spoiler js-faq-rotate"><span class="faq__symbol ">+</span></div> 
-										<span class="faq__text"><?php echo $questionFAQ ?> </span>
-									</a>
-									<div class="tab-content faq__content js-faq-content">
-										<p>
-											<?php echo $answerFAQ ?> 
-										</p>
-									</div>
-								</div>
-							</div> -->
 						<div class="b-faq">
 							<div class="tab faq__item">
 								<a href="" class="faq__title js-faq-title">
-									<div class="faq__spoiler js-faq-rotate"><span class="faq__symbol ">+</span></div> 
-									<span class="faq__text"><?php echo $questionFAQ ?></span>
+									<div class="faq__spoiler js-faq-rotate">
+										
+									</div> 
+									<span class="faq__text">
+										<div class="faq__symbol">
+											<button>
+												<span></span>
+												<span></span>
+											</button>
+										</div>
+										<?php echo $questionFAQ ?>
+									</span>
 								</a>
 								<div class="faq__content js-faq-content" style="display: none;">
 									<?php echo $answerFAQ ?> 
@@ -67,37 +60,3 @@
         <?php endif; ?>
     </div>
 </section>
-	
-	
-		<!-- FAQ SECTION 
-			<div class="b-faq">
-				<div class="faq__item">
-					<a href="#" class="faq__title js-faq-title">
-						<div class="faq__spoiler js-faq-rotate"><span class="faq__symbol ">+</span></div> 
-						<span class="faq__text">Заголовок не очень длинный</span>
-					</a>
-					<div class="faq__content js-faq-content">
-						Здесь контент<br/>
-            Здесь контент<br/>
-            Здесь контент<br/>
-            Здесь контент<br/>
-            Здесь контент<br/>
-            Здесь контент<br/>
-					</div>
-				</div>
-			</div>
-		<!-- FAQ SECTION END -->
-		<!-- FAQ SECTION 
-			<div class="b-faq">
-				<div class="faq__item">
-					<a href="#" class="faq__title js-faq-title">
-						<div class="faq__spoiler js-faq-rotate"><span class="faq__symbol ">+</span></div> 
-						<span class="faq__text">Заголовок не очень длинный</span>
-					</a>
-					<div class="faq__content js-faq-content">
-						Здесь контент
-					</div>
-				</div>
-			</div>
-		<!-- FAQ SECTION END -->
-<!-- END -->
