b39994ebe6b413409165c1952946c4b6370fb53c
OmelchenkoSerhii <87645842+OmelchenkoSerhii@users.noreply.github.com>
2021-09-24 15:28:50 +0300

    Update archive-proffesionals.php

--- archive-proffesionals.php
+++ archive-proffesionals.php
@@ -8,27 +8,32 @@
             <div class="page__heading">Статті для фахівців</div>
             <ul class="professionals-post__list">
                 <?php
-                    $args = array(
-                        'post_type' => 'post'
-                    );
-                    $query = new WP_Query('cat=46');
-                    $my_posts = $query->query($args);
 
-                    foreach( $my_posts as $my_post ){?>
-                        <li class="professionals-post row">
-                            <div class="professionals-post__imageWrapper col-lg-4">
-                                <img src="<?php the_post_thumbnail_url(); ?>" alt="">
-                            </div>
-                            <div class="professionals-post__contentWrapper col-lg-8">
-                                <p>
-                                    <strong><?php the_title(); ?></strong>
-                                    <p class="professionals-post__date"><?php echo get_the_date('j F Y'); ?></p>
-                                    <p  class="professionals-post__content"><?php echo the_excerpt(); ?></p>
-                                    <a href="<?php echo get_the_permalink(); ?>" class="professionals-post__button">Читать больше </a>
-                                </p>
-                            </div>
-                        </li>
-                    <?php } ?>
+$query = new WP_Query('cat=46');
+
+// Цикл
+if ( $query->have_posts() ) {
+	while ( $query->have_posts() ) {
+		$query->the_post();?>
+		 <li class="professionals-post row">
+               <div class="professionals-post__imageWrapper col-lg-4">
+                     <img src="<?php the_post_thumbnail_url(); ?>" alt="">
+                </div>
+                 <div class="professionals-post__contentWrapper col-lg-8">
+                   <p>
+					   <strong><?php the_title(); ?></strong>
+					   <p class="professionals-post__date"><?php echo get_the_date('j F Y'); ?></p>
+					   <p  class="professionals-post__content"><?php echo the_excerpt(); ?></p>
+					   <a href="<?php echo get_the_permalink(); ?>" class="professionals-post__button">Читать больше </a>
+                  </p>
+                </div>
+         </li>
+<?php	}
+} else {
+	// Постов не найдено
+}
+// Возвращаем оригинальные данные поста. Сбрасываем $post.
+wp_reset_postdata();?>
             </ul>
         </div>
     </section>
