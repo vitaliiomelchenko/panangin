f0033deb6c36695be2e35e3e880335883986715a
KorniienkoIvan <kornienkoivan462@gmail.com>
2021-09-24 17:08:26 +0300

    video fix

--- template-parts/acf-blocks/video-block.php
+++ template-parts/acf-blocks/video-block.php
@@ -1,8 +1,9 @@
+<?php $poster = get_sub_field('poster'); ?>
+<?php $video = get_sub_field('video'); ?>
+<?php if($video): ?>
 <section class="video-block">
     <div class="container">
         <div class="video__wrapper">
-            <?php $poster = get_sub_field('poster'); ?>
-            <?php $video = get_sub_field('video'); ?>
             <video id="player" playsinline controls <?php if($poster): ?>poster="<?php echo $poster['url'] ?>"<?php endif; ?>>
                 <source src="<?php echo $video['url'] ?>" type="video/mp4" />
             </video>
@@ -12,3 +13,4 @@
 <script>
   const player = new Plyr('#player');
 </script>
+<?php endif; ?>
