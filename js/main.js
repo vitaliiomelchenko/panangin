375caafafa42e934043d3f4665321bdd185e3435
KorniienkoIvan <kornienkoivan462@gmail.com>
2021-09-30 12:24:33 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -115,21 +115,20 @@ $(document).ready(function() {
 
 
 //smooth scrolling
-$(document).ready(function(){
+$(document).ready(function() {
   $("a").on('click', function(event) {
 
     if (this.hash !== "") {
-      
-      event.preventDefault();
 
       var hash = this.hash;
 
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 500, function() {
 
         window.location.hash = hash;
       });
+      return false;
     } 
   });
 });
