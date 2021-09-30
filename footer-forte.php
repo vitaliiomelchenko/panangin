95d4334b4eb053877605092ce6ff4120b90ddb12
OmelchenkoSerhii <87645842+OmelchenkoSerhii@users.noreply.github.com>
2021-09-30 13:57:42 +0300

    форте

--- footer-forte.php
+++ footer-forte.php
@@ -1,14 +1,10 @@
+<?php 
+    $forteFooter = get_field('forteFooter','option');
+?>
 <section class="forte__footer">
         <div class="container">
-            <p>
-                <strong><?php printf( _e('Корисні поради', 'panangin'))?></strong><br>
-                Перед застосуванням препарату рекомендується ознайомитися з інструкцією і проконсультуватися з лікарем. Р.С. МОЗ України<br> № UA / 7315/01/01 Термін дії посвідчення 01.08.2017 р<br>
-                <br>
-                Представництво «Ріхтер Гедеон Нрт.» в Україні www.richter.com.ua<br>
-                <br>
-                Адреса: 01054, м.Київ, вул.Тургенєвська, 17-б
-            </p>
-            <div class="forte__info">
+           <?php echo $forteFooter ?> 
+            <a href="https://levelmedia.com.ua/" class="forte__info">
                 Created by Level Media
             </div>
         </div>
