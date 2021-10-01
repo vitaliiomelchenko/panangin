1bc33507d123b15bd0b939a137f31e485a835ae7
OmelchenkoSerhii <87645842+OmelchenkoSerhii@users.noreply.github.com>
2021-10-01 17:18:15 +0300

    warning

--- template-parts/acf-blocks/warning.php
+++ template-parts/acf-blocks/warning.php
@@ -1,10 +1,12 @@
 <?php
     $warning = get_sub_field('warning');
+    $warningMobile = get_sub_field('warningMobile');
 ?>
 <?php if( !empty( $warning ) ): ?>
     <section class="warning">
         <div class="container">
-            <img src="<?php echo esc_url($warning['url']); ?>" alt="<?php echo esc_attr($warning['alt']); ?>" />
+            <img class="warning-img" src="<?php echo esc_url($warning['url']); ?>" alt="<?php echo esc_attr($warning['alt']); ?>" />
+            <img class="warning-img-mobile" src="<?php echo esc_url($warningMobile['url']); ?>" alt="<?php echo esc_attr($warningMobile['alt']); ?>" />
         </div>
     </section> 
 <?php endif; ?>
