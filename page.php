ba50690a1c486a77ed4e90e0312662dcc0e9f162
OmelchenkoSerhii <87645842+OmelchenkoSerhii@users.noreply.github.com>
2021-10-04 16:50:17 +0300

    s

--- page.php
+++ page.php
@@ -15,12 +15,22 @@
             </div>
         </div>
     </section>
-    <section class="blog">
-    <div class="container">
-        <div class="blog__heading">
-            <h1>Корисні поради</h1>
-        </div>
-        <ul class="row blog__row">
+    <section class="posts_block">
+    <?php 
+    if( get_locale() == 'uk'){
+        $blockTitle = 'Корисні поради';
+        $blockButton = 'Всі новини';
+        $postButton = 'Читати далі';
+    }   
+    elseif(get_locale() == 'ru_RU'){
+        $blockTitle = 'Полезные советы';
+        $blockButton = 'Все новости';
+        $postButton = 'Читать дальше';
+    }
+    ?>
+    <div class="page__heading"><?php echo $blockTitle; ?></div>
+        <div class="container">
+            <ul class="row blog__row">
 
                 <?php 
                 $args = array(
@@ -38,7 +48,7 @@
                                         <h5>
                                             <?php the_title(); ?>
                                         </h5>
-                                        <a class="blog-button" href="<?php echo get_the_permalink(); ?>">Читати далі <svg width="17" height="15"
+                                        <a class="blog-button" href="<?php echo get_the_permalink(); ?>"><?php echo $postButton; ?><svg width="17" height="15"
                                                 viewBox="0 0 17 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M15.75 7.72559L0.75 7.72559" stroke="#58585B" stroke-width="1.5"
                                                     stroke-linecap="round" stroke-linejoin="round" />
@@ -55,9 +65,10 @@
                 <p><?php esc_html_e( 'Нет постов по вашим критериям.' ); ?></p>
             <?php endif; ?>                
         </ul>
-        <div class="blog__buttonWrapper">
-            <a href="https://panangin.levelmedia.com.ua/korisni-poradi/" class="button button-primary">Всі новини</a>
+        <div class="button-wrapper">
+            <a href="https://panangin.levelmedia.com.ua/korisni-poradi/" class="button button-primary"><?php echo $blockButton; ?></a>
         </div>
     </div>
 </section>
+
 <?php get_footer(); ?>
